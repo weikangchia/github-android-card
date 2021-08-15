2bfa1b31969d2c5320dfdf1cd905339977066187
Wei Kang <weikangchia@gmail.com>
2021-08-15 21:54:27 +0800

    fix api

--- api/gplays.ts
+++ api/gplays.ts
@@ -4,10 +4,11 @@ import gplay from 'google-play-scraper';
 export default (_req: VercelRequest, res: VercelResponse) => {
   const id =  String(_req.query.id);
 
+  console.log(`id: ${id}`);
+
   let responseData = null;
   gplay.app({appId: id}).then((data) => {
     responseData = data;
+    res.status(200).send(responseData);
   }, console.log);
-
-  res.status(200).send(responseData);
 };
