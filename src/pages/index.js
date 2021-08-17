519b179965918109e9c4cc43f595ddbd30e21a1c
Wei Kang <weikangchia@gmail.com>
2021-08-17 13:35:23 +0800

    update index.js and remove date.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import '../styles/index.css';
 
 function Index() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
       <Helmet>
         <title>GitHub Android Card</title>
       </Helmet>
-      <h1>GitHub Android Card</h1>
+      <h1><a href="https://github.com/weikangchia/github-android-card">GitHub Android Card</a></h1>
       <h2>
         Deployed with{' '}
         <a
@@ -29,12 +20,12 @@ function Index() {
         </a>
         !
       </h2>
-      <p>
-        
-      </p>
-      <br />
+      <h3>To display it in your GitHub README file</h3>
+      
+      <p>![Readme Card](https://github-android-card.vercel.app/api/gplay-apps/com.appspot.khubite.sgsupermarket)</p>
+      
       <h2>Sample</h2>
-      <img alt="github android card sample" style={{width: "400px", height: "160px", display: "block", "marginLeft": "auto", "marginRight": "auto"}} src="https://github-android-card.vercel.app/api/gplay-apps/khubite.appspot.com.sgtvguide"></img>
+      <img alt="github android card sample" style={{width: "400px", height: "160px", display: "block", "marginLeft": "auto", "marginRight": "auto"}} src="http://localhost:3000/api/gplay-apps/khubite.appspot.com.sgtvguide"></img>
     </main>
   );
 }
